f3fed76bc9d31ee670ac5ca43c6cc8fe961658eb
newtratip <tharathip.chaweewongphan@gmail.com>
2019-12-21 11:10:21 +0700

    [13.0][UPD] partner_contact_birthdate

--- partner_contact_birthdate/models/__init__.py
+++ partner_contact_birthdate/models/__init__.py
@@ -1,1 +1,5 @@
+# Copyright 2014-2015  Grupo ESOC <www.grupoesoc.es>
+# Copyright 2017-Apertoso N.V. (<http://www.apertoso.be>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from . import res_partner
