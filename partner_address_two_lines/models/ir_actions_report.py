1c48038c401bc470b8269678392d8409ec5ddb78
Tonow-c2c <thomas.nowicki@camptocamp.com>
2020-01-27 16:01:46 +0100

    [ADD][12.0] add partner_address_two_lines module

---
+++ partner_address_two_lines/models/ir_actions_report.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class IrActionsReport(models.Model):
+    _inherit = "ir.actions.report"
+
+    @api.model
+    def _get_rendering_context(self, docids, data):
+        """ Set context key to split partner address on two line only on report
+        """
+        res = super(IrActionsReport, self)._get_rendering_context(docids, data)
+        docs = res.get("docs")
+        res["docs"] = docs.with_context(_two_lines_partner_address=True)
+        return res
